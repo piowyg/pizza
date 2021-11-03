d26a5c6582e69d7e03c00ae1b90ff532e7ddc787
piowyg <piowyg@st.amu.edu.pl>
2021-11-04 00:12:43 +0100

    small fixes with paths and text changes

--- src/pages/PizzaPage.js
+++ src/pages/PizzaPage.js
@@ -6,9 +6,9 @@ import Product from '../components/Product'
 const PizzaPage = ({ match }) => {
   return (
     <>
-      <div>Strona produktu</div>
+      <div>Strona pizzy</div>
       <Product id={match.params.id} />
-      <Link to="/products">Powrót do listy produktów</Link>
+      <Link to="/products">Powrót do listy pizz</Link>
     </>
 
   );
