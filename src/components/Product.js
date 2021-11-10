2e9cd1fd67ebc44b942e0378e1532e96831f0993
piowyg <piowyg@st.amu.edu.pl>
2021-11-10 23:37:36 +0100

    added styles for pizza page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import '../styles/Product.css';
+import { useSelector } from 'react-redux';
 import { translations } from '../dictionary/transaltions';
+import cart_pizza_list from '../images/cart.png';
 
 const Product = (props) => {
   const pizza = "/pizze/" + props.id + ".png";
+
+  const pizzas = useSelector((state) => state.pizzas);
+  const ingredients_store = useSelector((state) => state.ingredients);
+
+  let current_pizza = []
+
+  pizzas.forEach(element => {
+              if (element.name === props.id) {
+                current_pizza = element;
+              }
+            });
+
+  const list = current_pizza.ingredients.map((product, index) => {
+    return (
+    <li key={product.id}>
+        {
+          ingredients_store.map(element => {
+            if (element.id === product) {
+              return element.name  
+            }
+            }
+        )}
+    </li>)
+  })
+
   return (
+    <>
     <article className="product">
       <h1>{props.id}</h1>
       <article> {translations[props.id]}</article>
       <img src={pizza} alt={props.id} className="pizza_img"/>
+
+      <h2>Składniki</h2>
+      <ul className="ingredients_pizza_page">
+        {list}
+      </ul>
+      <button className="add_ingredient">Dodaj składnik </button>
+
+      <button className="add_ingredient"> <span>{current_pizza.price} zł</span> <img src={cart_pizza_list} alt="koszyk"></img></button>
     </article>
+
+  
+  </>
   );
 }
 
