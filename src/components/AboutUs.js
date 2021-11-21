7034a4fdcd982d626ec63cabfd46d3c9de48045c
Piotr Wygocki <piotrwygocki@MacBook-Pro-Piotr.local>
2021-11-21 22:07:10 +0100

    added video to about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,6 +2,10 @@ const AboutUs = () => {
     return (
         <div>
             <h2> Witamy w najlepszej pizzeri</h2>
+            <br/>
+            <h2>O to jak nasi kucharze przygotowywują twoją pizzę</h2>
+            <br/>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/JSLXP4XCMBc" title="Magiczna Pizza" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true" webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
         </div>
     )
 }
